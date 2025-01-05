6a94d152630ab996ad98b8004b70c5073d1235ef
Filip Kampić <filipkampic1@gmail.com>
2025-01-05 13:38:55 +0100

    Add LaunchedEffect in SplashScreen and Add Preview for SplashScreen

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 android {
     namespace = "com.orwima.premierleaguearchitect"
-    compileSdk = 34
+    compileSdk = 35
 
     defaultConfig {
         applicationId = "com.orwima.premierleaguearchitect"
@@ -49,6 +49,7 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
+    implementation(libs.androidx.navigation.runtime.ktx)
     testImplementation(libs.junit)
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
@@ -56,4 +57,5 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+    implementation("androidx.navigation:navigation-compose:2.7.3")
 }
--- app/src/main/java/com/orwima/premierleaguearchitect/SplashScreen.kt
+++ app/src/main/java/com/orwima/premierleaguearchitect/SplashScreen.kt
@@ -1,2 +1,28 @@
 package com.orwima.premierleaguearchitect
 
+import androidx.compose.foundation.layout.Box
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavController
+import androidx.navigation.compose.rememberNavController
+import kotlinx.coroutines.delay
+
+@Composable
+@Preview(showBackground = true)
+fun SplashScreenPreview() {
+    SplashScreen(navController = rememberNavController())
+}
+
+@Composable
+fun SplashScreen(
+    navController: NavController
+) {
+    LaunchedEffect(Unit) {
+        delay(3000)
+        navController.navigate("home") {
+            popUpTo("splash") { inclusive = true } //  Ensures the splash screen is completely removed from the back stack, preventing the user from going back to it.
+        }
+    }
+}
