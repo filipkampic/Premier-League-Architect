d281d75e09bc2ea415dcbbf03dbf49ae36790373
Filip Kampić <filipkampic1@gmail.com>
2025-01-05 14:17:00 +0100

    Add Content in SplashScreen

---
+++ app/src/main/java/com/orwima/premierleaguearchitect/Typography.kt
@@ -0,0 +1,9 @@
+package com.orwima.premierleaguearchitect
+
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.font.FontWeight
+
+val BebasNeueFontFamily = FontFamily(
+    Font(R.font.bebas_neue, FontWeight.Normal)
+)
