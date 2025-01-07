401c187878d2477c3b63537a524efff75ddfb5df
Filip Kampić <filipkampic1@gmail.com>
2025-01-07 23:07:16 +0100

    Add App Navigation to navigate between activities

---
+++ app/src/main/java/com/orwima/premierleaguearchitect/AppNavigation.kt
@@ -0,0 +1,19 @@
+package com.orwima.premierleaguearchitect
+
+import androidx.compose.runtime.Composable
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
+
+@Composable
+fun AppNavigation() {
+    val navController = rememberNavController()
+
+    NavHost(
+        navController = navController,
+        startDestination = "splash"
+    ) {
+        composable("splash") { SplashScreen(navController = navController) }
+        composable("home") { HomeScreen(navController = navController) }
+    }
+}
