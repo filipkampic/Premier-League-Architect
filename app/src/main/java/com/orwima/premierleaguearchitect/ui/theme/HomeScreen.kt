c030a1bcc8500ac4314a290883f764b11793fa44
Filip Kampić <filipkampic1@gmail.com>
2025-01-07 19:07:11 +0100

    Finish main box in HomeScreen and start making HomeButton

--- app/src/main/java/com/orwima/premierleaguearchitect/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/orwima/premierleaguearchitect/ui/theme/HomeScreen.kt
@@ -2,16 +2,29 @@ package com.orwima.premierleaguearchitect.ui.theme
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.scale
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.layout.ContentScale
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.Font
+import androidx.compose.ui.text.font.FontFamily
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
 import com.orwima.premierleaguearchitect.R
@@ -39,6 +52,56 @@ fun HomeScreen(
                 .fillMaxSize()
                 .graphicsLayer(alpha = 0.4f)
                 .scale(3.2f)
+        )
+
+        Box(
+            modifier = Modifier
+                .fillMaxSize()
+        ) {
+            Text(
+                text = "PREMIER LEAGUE ARCHITECT",
+                fontFamily = FontFamily(Font(R.font.bebas_neue)),
+                fontSize = 56.sp,
+                color = Color(0XFF00FF85),
+                textAlign = TextAlign.Center,
+                modifier = Modifier
+                    .align(Alignment.TopCenter)
+                    .padding(top = 80.dp)
+            )
+
+            HomeButton(text = "CREATE LINEUP") {
+                navController.navigate("team_selection")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            HomeButton(text = "SAVED LINEUPS") {
+                navController.navigate("saved_lineups")
+            }
+
+            Spacer(modifier = Modifier.height(16.dp))
+
+            HomeButton(text = "LINEUPS BY TEAM") {
+                navController.navigate("lineups_by_team")
+            }
+        }
+    }
+}
+
+@Composable
+fun HomeButton(
+    text: String,
+    onClick: () -> Unit
+) {
+    Box(
+        modifier = Modifier
+            .fillMaxWidth(0.8f)
+            .height(50.dp)
+            .background(Color.White, shape = RoundedCornerShape(10.dp))
+            .clickable { onClick() },
+    ) {
+        Text(
+            text = text
         )
     }
 }
