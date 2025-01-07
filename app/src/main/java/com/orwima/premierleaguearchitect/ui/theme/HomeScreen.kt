7f22e4c3e3927b21f219b6f2babc2a90b0ece320
Filip Kampić <filipkampic1@gmail.com>
2025-01-07 18:25:24 +0100

    Add background color and image in HomeScreen

--- app/src/main/java/com/orwima/premierleaguearchitect/ui/theme/HomeScreen.kt
+++ app/src/main/java/com/orwima/premierleaguearchitect/ui/theme/HomeScreen.kt
@@ -1,10 +1,20 @@
 package com.orwima.premierleaguearchitect.ui.theme
 
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.graphicsLayer
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.orwima.premierleaguearchitect.R
 
 @Composable
 @Preview(showBackground = true)
@@ -16,5 +26,19 @@ fun HomeScreenPreview(modifier: Modifier = Modifier) {
 fun HomeScreen(
     navController: NavController
 ) {
-
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(color = Color(0XFF252431))
+    ) {
+        Image(
+            painter = painterResource(id = R.drawable.pl_bg),
+            contentDescription = "Premier League Logo Background",
+            contentScale = ContentScale.Fit,
+            modifier = Modifier
+                .fillMaxSize()
+                .graphicsLayer(alpha = 0.4f)
+                .scale(3.2f)
+        )
+    }
 }
